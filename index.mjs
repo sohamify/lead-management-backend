6f0c30e93c5ce97ae557734551935cba51b88540
sohamify <sohampanji123@gmail.com>
2025-09-18 17:49:40 +0530

    frontend url added to the cors origin policy

--- index.mjs
+++ index.mjs
@@ -14,7 +14,7 @@ const app = express();
 
 // middlewares setup
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'https://lead-management-frontend-sage.vercel.app/',
   credentials: true,
 }));
 app.use(express.json());
